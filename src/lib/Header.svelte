4aed90eccf74e6be9c87d50ed5b6de364d704943
Markus Haubold <105038675+haub94@users.noreply.github.com>
2023-04-04 18:06:27 +0200

    init sveltekit and tailwind

---
+++ src/lib/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let publicVar = "defaultVaule";
+  let var1: string = "text_var";
+
+  function hello(): number {
+    console.log("hello :>> ");
+    return 1;
+  }
+</script>
+
+<div class="flex flex-row bg-black p-5 text-3xl text-white mx-auto py-4 md:py-4 md:flex-row">
+  <p>NFTW Adventures</p>
+  <a href="/impressum" class="ml-48 text-lg bg-red-600">Impressum</a>
+  <button class="ml-12 text-lg">spacetravel</button>
+  <button class="ml-12 text-lg">client storys</button>
+  <button class="ml-12 text-lg">{publicVar}</button>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Footer from "$lib/Footer.svelte";
+    
+    import Header from "$lib/Header.svelte";
+
+    
+
+    import "../app.css";
+
+
+</script>
+
+<Header publicVar="Wert aus layout"/>
+
+<div class="bg-green-600 h-24">
+    <slot />
+</div>
+
+
+<Footer />
+
