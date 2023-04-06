1786900e837e88e1d6b5ae31ecd215db5da4c410
Markus Haubold <105038675+haub94@users.noreply.github.com>
2023-04-07 01:20:06 +0200

    fix dynamic component loading

--- src/lib/dev/docu/Components.svelte
+++ src/lib/dev/docu/Components.svelte
@@ -1,43 +1,37 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	const jsonFile: string = '/content.json';
+
+	//add all component-imports here
+	import Header from '$lib/sectionComponents/Header.svelte';
+	import Footer from '$lib/sectionComponents/Footer.svelte';
 
 
-
-	export let name = "Button";
-	export let description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et"
-
-
+	//fill with docu-data
+	const components = [
+		{ 
+			name: 'Header',   
+			component: Header,
+			description: 'Beschreibung zu Header.',
+			"props": ["eins", "zwei", "drei"]   
+		},
+		{ 
+			name: 'Footer', 
+			component: Footer,
+			description: 'Beschreibung zu Footer.',
+			"props": ["eins", "zwei", "drei"] 
+		},
+	];
 	
-
-	onMount(() => {
-		fetch(jsonFile)
-			.then(function (response) {
-				return response.json();
-			})
-			.then(function (data) {
-				let content = JSON.parse(JSON.stringify(data));
-				console.log('content :>> ', content);
-			
-				//console.log("eintrag: " + content.day[0].monday[0].town);
-			})
-		.catch(function (e) {
-			console.log('error creating location-data: ' + e);
-		});
-	});
-
-
-
 </script>
 
-<!--
-{#each data as dat}
+
+
+{#each components as component}
 	<main>
 		<div class="py-6">
 			<div class="flex flex-row justify-between content-center mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
 				<div class='flex flex-col space-y-2'>
-					<h1 class="h-12 text-2xl font-semibold text-gray-900">{name}</h1>
-					<p>{description}</p>
+					<h1 class="h-12 text-2xl font-semibold text-gray-900">{component.name}</h1>
+					<p>{component.description}</p>
 				</div>
 
 				<button class="h-12 w-40 ml-12 text-blue-500 bg-transparent border border-solid border-blue-500 hover:bg-blue-500 hover:text-white active:bg-blue-600 font-bold uppercase text-sm rounded outline-none focus:outline-none ease-linear transition-all duration-150  active show px-3">
@@ -47,13 +41,11 @@
 			<div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
 				<div class="py-4 h-auto">
 						<div class="flex justify-center my-4 py-4 border-2 border-gray-300 rounded-lg">
-							<svelte:component this={Button} />
-
-
+							<svelte:component this={component.component}/>
 						</div>
 				</div>
 			</div>
 		</div>
 	</main>
 {/each}
--->
+
