18ca2b74ad9828ad8669d27ae6336e17985a6775
Markus Haubold <105038675+haub94@users.noreply.github.com>
2023-04-06 22:37:02 +0200

    change route structure to get flexible layouts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,5 @@
 <script>
-    import Footer from "$lib/sectionComponents/Footer.svelte";
-    
-    import Header from "$lib/sectionComponents/Header.svelte";
-
     import "../app.css";
 </script>
 
-
-<Header />
-
 <slot />
-
-<Footer />
-
