d3a6281aa7ff66d74bd5e10981c300298a237f9f
Markus Haubold <105038675+haub94@users.noreply.github.com>
2023-04-08 01:11:38 +0200

    add code preview

--- src/lib/dev/docu/Components.svelte
+++ src/lib/dev/docu/Components.svelte
@@ -1,60 +1,107 @@
 <script lang="ts">
+    import ScriptBlock from "./ScriptBlock.svelte";
+    //add all component-imports here
+    import Header from "$lib/sectionComponents/Header.svelte";
+    import Footer from "$lib/sectionComponents/Footer.svelte";
 
-	//add all component-imports here
-	import Header from '$lib/sectionComponents/Header.svelte';
-	import Footer from '$lib/sectionComponents/Footer.svelte';
 
+  let previewState: boolean = false;
 
-	//fill with docu-data
-	const components = [
-		{ 
-			name: 'Header',   
-			component: Header,
-			description: 'Beschreibung zu Header.',
-			parameter: ["eins", "zwei", "drei"],
-			parameterDescription: [],  
-		},
-		{ 
-			name: 'Footer', 
-			component: Footer,
-			description: 'Beschreibung zu Footer.',
-			parameter: ["eins", "zwei", "drei"],
-			parameterDescription: [],    
-		},
-	];
+  function togglePreviewState() {
+    previewState = !previewState;
+  }
 
-	
-	
+  //fill with docu-data
+  const components = [
+    {
+      name: "Header",
+      component: Header,
+      description: "Beschreibung zu Header.",
+      parameter: ["eins", "zwei", "drei"],
+      parameterDescription: [],
+      script: 
+`
+  function helloWorld() {
+  console.log("hello");
 
+  return(0);
+}`,
+      html: 
+      `
+<div class="flex flex-row bg-black p-5 text-3xl text-white mx-auto py-4 md:py-4 md:flex-row">
+  <p>NFTW Adventures</p>
+  <a href="/impressum" class="ml-48 text-lg bg-red-600">Impressum</a>
+  <button class="ml-12 text-lg">spacetravel</button>
+  <button class="ml-12 text-lg">client storys</button>
+</div>
+	  `
+    },
+    {
+      name: "Footer",
+      component: Footer,
+      description: "Beschreibung zu Footer.",
+      parameter: ["eins", "zwei", "drei"],
+      parameterDescription: [],
+      script: ``,
+      html: 
+      `
+      <div>test</div>
+      `,
+    },
+  ];
 
 
 
 
 </script>
 
+<div class="[&>*:nth-child(even)]:bg-gray-200 [&>*:nth-child(odd)]:bg-gray-50">
+  {#each components as component}
+    <div class="py-6">
+      <div
+        class="mx-auto flex max-w-7xl flex-row content-center justify-between px-4 sm:px-6 md:px-8">
+        <div class="flex flex-col space-y-2">
+          <h1 class="h-12 text-2xl font-semibold text-gray-900">
+            {component.name}
+          </h1>
+          <p>{component.description}</p>
+        </div>
 
-<div class="[&>*:nth-child(odd)]:bg-gray-50 [&>*:nth-child(even)]:bg-gray-200">
-	{#each components as component}
-		<div class="py-6">
-			<div class="flex flex-row justify-between content-center mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-				<div class='flex flex-col space-y-2'>
-					<h1 class="h-12 text-2xl font-semibold text-gray-900">{component.name}</h1>
-					<p>{component.description}</p>
-				</div>
+        <!--toggle preview-->
+        <button
+          on:click={togglePreviewState}
+          class="active show ml-12 h-12 w-40 rounded border border-solid border-blue-500 bg-transparent px-3 text-sm font-bold uppercase text-blue-500 outline-none transition-all duration-150 ease-linear hover:bg-blue-500 hover:text-white focus:outline-none active:bg-blue-600">
+          show code
+        </button>
+      </div>
 
-				<button class="h-12 w-40 ml-12 text-blue-500 bg-transparent border border-solid border-blue-500 hover:bg-blue-500 hover:text-white active:bg-blue-600 font-bold uppercase text-sm rounded outline-none focus:outline-none ease-linear transition-all duration-150  active show px-3">
-					show code
-				</button>
-			</div>
-			<div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-				<div class="py-4 h-auto">
-						<div class="flex justify-center my-4 p-4 border-2 border-gray-300 rounded-lg">
-							<svelte:component this={component.component}/>
-						</div>
-				</div>
-			</div>
-		</div>
-	{/each}
+      <!--preview-->
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+          <div class="h-auto py-4">
+            <div class="my-4 flex justify-center rounded-lg border-2 border-gray-300 p-4">
+              {#if !previewState} 
+				      <!--component preview-->  
+              <svelte:component this={component.component} />    
+              {:else}
+                <!--code preview-->
+                <div class="flex-row">
+                  {#if component.script !== ``}
+                    <pre>
+                        <ScriptBlock>
+                          <slot>
+                            {component.script}
+                          </slot>
+                        </ScriptBlock>  
+                    </pre>
+                  {/if}
+                  <pre>
+                    {component.html}
+                  </pre>
+              </div> 
+			        {/if}
+			      </div>
+          </div>
+        </div>
+    </div>
+  {/each}
 </div>
-
-
---
+++ src/lib/dev/docu/ScriptBlock.svelte
@@ -0,0 +1,12 @@
+<script>
+    let scriptOpen = `<script`;
+    let arrowRight = `>`;
+    let arrowLeft = `<`;
+    let scriptClosed = `/script>`
+</script>
+
+<div>
+<span>{scriptOpen}</span> <span>lang="ts"</span><span>{arrowRight}</span>
+<slot /> <br>
+<span>{arrowLeft}</span><span>{scriptClosed}</span>
+</div>
