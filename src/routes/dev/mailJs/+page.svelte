9a171bad4b8b30d6770a1c1ff2c5699ca5a74ab6
Markus Haubold <105038675+haub94@users.noreply.github.com>
2023-04-13 13:21:36 +0200

    cleanup and set logs only during dev

--- src/routes/dev/mailJs/+page.svelte
+++ src/routes/dev/mailJs/+page.svelte
@@ -1,24 +1,26 @@
 <script lang="ts">
     import emailjs from '@emailjs/browser';
+    import { dev } from '$app/environment';
   
     let userName: string;
     let toMail: string;
 
-    function log() {
-        console.log('userName :>> ', userName);
-        console.log('toMail :>> ', toMail);
+    function mailingSequence() {
+        if (dev) {
+            console.log("mailing data:")
+            console.log('userName :>> ', userName);
+            console.log('toMail :>> ', toMail);
+
+            console.log("call emailJS function to send mail");
+        }
 
         sendEmail(userName, toMail);
     }
 
     function sendEmail(name: string, mail: string) {
-        console.log("sending mail in progress...");
-
-        console.log('sendMail: userName :>> ', name);
-        console.log('sendMail:  toMail :>> ', mail);
-
         emailjs.send('service_vzyr2ok', 
         'template_3sdt4dk', 
+        //template variables
         {
             from_name: name,
             to_mail: mail
@@ -26,23 +28,21 @@
         'YYaLHQ2Bd6V9Rk4vS')
         
         .then((result) => {
-            console.log('SUCCESS!', result.text);
+           //trigger here green success banner!!!
+            if (dev) {
+                console.log('SUCCESS!', result.text);
+            }
         }, (error) => {
-            console.log('FAILED...', error.text);
+            //trigger here red error banner!!!
+            if (dev) {
+                console.log('FAILED...', error.text);
+            } 
         });
     }
 
-/*
-      emailjs.sendForm('service_vzyr2ok', 'template_3sdt4dk', e.target, 'YYaLHQ2Bd6V9Rk4vS')
-        .then((result) => {
-            console.log('SUCCESS!', result.text);
-        }, (error) => {
-            console.log('FAILED...', error.text);
-        }); 
-    }*/
   </script>
   
-  <form on:submit|preventDefault={log}>
+  <form on:submit|preventDefault={mailingSequence}>
     <label>Name</label>
     <input type="text" name="user_name" bind:value={userName}>
     <label>Email</label>
