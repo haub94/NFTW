9889db8b6930c94770505be146808243ac60b193
Markus Haubold <105038675+haub94@users.noreply.github.com>
2023-04-13 12:36:08 +0200

    form works with bind:value

--- src/routes/dev/mailJs/+page.svelte
+++ src/routes/dev/mailJs/+page.svelte
@@ -1,12 +1,27 @@
-<script>
+<script lang="ts">
     import emailjs from '@emailjs/browser';
   
-    function sendEmail(e) {
+    let userName: string;
+    let toMail: string;
+
+    function log() {
+        console.log('userName :>> ', userName);
+        console.log('toMail :>> ', toMail);
+
+        sendEmail(userName, toMail);
+    }
+
+    function sendEmail(name: string, mail: string) {
+        console.log("sending mail in progress...");
+
+        console.log('sendMail: userName :>> ', name);
+        console.log('sendMail:  toMail :>> ', mail);
+
         emailjs.send('service_vzyr2ok', 
         'template_3sdt4dk', 
         {
-            from_name: "markus",
-            to_mail: "haubie94@web.de"
+            from_name: name,
+            to_mail: mail
         }, 
         'YYaLHQ2Bd6V9Rk4vS')
         
@@ -27,11 +42,11 @@
     }*/
   </script>
   
-  <form on:submit|preventDefault={sendEmail}>
+  <form on:submit|preventDefault={log}>
     <label>Name</label>
-    <input type="text" name="user_name">
+    <input type="text" name="user_name" bind:value={userName}>
     <label>Email</label>
-    <input type="email" name="user_email">
+    <input type="text" name="user_email" bind:value={toMail}>
     <label>Message</label>
     <textarea name="message"></textarea>
     <input type="submit" value="Send">
