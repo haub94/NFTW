a19855429453a2cff0ad9b2d119ac2bb32e4c4ed
Markus Haubold <105038675+haub94@users.noreply.github.com>
2023-04-16 01:04:16 +0200

    add content to the  docu startpage

--- src/routes/dev/docu/+page.svelte
+++ src/routes/dev/docu/+page.svelte
@@ -1,11 +1,17 @@
+<script>
+  import DiDropbox from 'svelte-icons/di/DiDropbox.svelte';
+  import FaCode from 'svelte-icons/fa/FaCode.svelte';
+  import FaPaintRoller from 'svelte-icons/fa/FaPaintRoller.svelte';
+</script>
+
 <div class="lg:pl-72 bg-NFTW-lila-800">
     <div class="w-full h-16 bg-bg-NFTW-lila-800 flex justify-center items-center">
         <h2 class="text-white font-bold text-2xl">Welcome</h2>
     </div>
-    <div class="p-4 sm:p-6 lg:p-8 rounded-tl-xl bg-white">
+    <div class="p-4 sm:p-6 lg:p-8 rounded-tl-xl bg-NFTW-blue-100">
         <!--here comes the content from the docu-->
         
-        <div class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+        <div class="relative isolate overflow-hidden bg-NFTW-blue-100 px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
             <div class="absolute inset-0 -z-10 overflow-hidden">
               <svg class="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
                 <defs>
@@ -13,7 +19,7 @@
                     <path d="M100 200V.5M.5 .5H200" fill="none" />
                   </pattern>
                 </defs>
-                <svg x="50%" y="-1" class="overflow-visible fill-gray-50">
+                <svg x="50%" y="-1" class="overflow-visible fill-NFTW-blue-100">
                   <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />
                 </svg>
                 <rect width="100%" height="100%" stroke-width="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
@@ -23,9 +29,9 @@
               <div class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
                 <div class="lg:pr-4">
                   <div class="lg:max-w-lg">
-                    <p class="text-base font-semibold leading-7 text-indigo-600">Develop faster</p>
+                    <p class="text-base font-semibold leading-7 text-NFTW-pink-500">Develop faster</p>
                     <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</h1>
-                    <p class="mt-6 text-xl leading-8 text-gray-700">Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.</p>
+                    <p class="mt-6 text-xl leading-8 text-gray-700">Welcome to the documentation page of the NFTW Adventures website. As a good developer, you should record any changes here.</p>
                   </div>
                 </div>
               </div>
@@ -35,31 +41,36 @@
               <div class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
                 <div class="lg:pr-4">
                   <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-                    <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
+                    <p>The documentation is divided into four sections: Components, Scripts, Database and Design. </p>
+                    
+
                     <ul role="list" class="mt-8 space-y-8 text-gray-600">
                       <li class="flex gap-x-3">
-                        <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                          <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clip-rule="evenodd" />
-                        </svg>
-                        <span><strong class="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</span>
+                        <div class="mt-1 h-8 w-8 flex-none text-NFTW-pink-500">
+                          <DiDropbox />
+                        </div>
+                        <span><strong class="font-semibold text-gray-900">Components. </strong>All components used on the site are listed in the first chapter. The information to be stored is provided with a template in the code at the appropriate place and in the about site. The template must be strictly adhered to and all data must be entered!</span>
                       </li>
                       <li class="flex gap-x-3">
-                        <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                          <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
-                        </svg>
-                        <span><strong class="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
+                        <div class="mt-1 h-8 w-8 flex-none text-NFTW-pink-500">
+                          <FaCode />
+                        </div>
+                        <span><strong class="font-semibold text-gray-900">TypeScript's. </strong>All scripts must be written without errors in Typescript and stored in text form in the documentation! The appropriate template can be found in the about site.</span>
                       </li>
                       <li class="flex gap-x-3">
-                        <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <svg class="mt-1 h-8 w-8 flex-none text-NFTW-pink-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                           <path d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z" />
                           <path fill-rule="evenodd" d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z" clip-rule="evenodd" />
                         </svg>
-                        <span><strong class="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</span>
+                        <span><strong class="font-semibold text-gray-900">Database. </strong>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</span>
+                      </li>
+                      <li class="flex gap-x-3">
+                        <div class="mt-1 h-8 w-8 flex-none text-NFTW-pink-500">
+                          <FaPaintRoller />
+                        </div>
+                        <span><strong class="font-semibold text-gray-900">Corporate Design. </strong>The corporate design to be used for the styling is provided in the last chapter. All changes to the design must be noted there!</span>
                       </li>
                     </ul>
-                    <p class="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.</p>
-                    <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
-                    <p class="mt-6">Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam.</p>
                   </div>
                 </div>
               </div>
