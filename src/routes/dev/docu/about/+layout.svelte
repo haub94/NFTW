814598d4378c958c72397c279bd5db831af41428
Markus Haubold <105038675+haub94@users.noreply.github.com>
2023-04-16 12:49:30 +0200

    change bg styling

--- src/routes/dev/docu/about/+layout.svelte
+++ src/routes/dev/docu/about/+layout.svelte
@@ -2,7 +2,7 @@
     <div class="w-full h-16 bg-NFTW-lila-800 flex justify-center items-center">
         <h2 class="text-white font-bold text-2xl">About this documentation</h2>
     </div>
-    <div class="p-4 sm:p-6 lg:p-8 rounded-tl-xl bg-white">
+    <div class="p-4 sm:p-6 lg:p-8 rounded-tl-xl bg-NFTW-blue-100 h-screen">
         <!--here comes the content from the docu-->
         <slot></slot>
     </div>
