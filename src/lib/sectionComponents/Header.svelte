b58d59dcfcbbd6bb8a8d3d20d8e963d9546b29ae
DanielRittrich <danielrittrich@web.de>
2023-04-21 16:47:30 +0200

    Header

--- src/lib/sectionComponents/Header.svelte
+++ src/lib/sectionComponents/Header.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-  import { fade, slide } from "svelte/transition";
-  import { sineIn, quintInOut } from "svelte/easing";
+  import { dev } from "$app/environment";
 
   export let publicVar = "defaultVaule";
   let var1: string = "text_var";
@@ -11,6 +10,26 @@
   }
 </script>
 
+{#if dev}
+  <div class="fixed top-1 left-3 z-50 flex text-white text-3xl gap-6">
+    <p class="sm:hidden">mobil</p>
+    <p class="hidden sm:block md:hidden">sm</p>
+    <p class="hidden md:block lg:hidden">md</p>
+    <p class="hidden lg:block xl:hidden">lg</p>
+    <p class="hidden xl:block">xl</p>
+    <a
+      href="/test"
+      class="pt-1 text-lg hover:scale-125 hover:transition-all hover:text-NFTW-pink-500 ease-in-out duration-300"
+      >Test</a
+    >
+    <a
+      href="/test2"
+      class="pt-1 text-lg hover:scale-125 hover:transition-all hover:text-NFTW-pink-500 ease-in-out duration-300"
+      >Test 2</a
+    >
+  </div>
+{/if}
+
 <div class="group header-bg h-40 overflow-hidden bg-center relative bg-cover">
   <img
     class="transition delay-50 ease-in-out group-hover:scale-150 group-hover:rotate-2 absolute"
@@ -18,55 +37,32 @@
     src="/milkyway.png"
     alt="milkyway"
   />
+
   <div
-    class="group flex flex-row bg-black opacity-80 p-5 text-3xl text-white mx-auto py-4 md:py-4 md:flex-row absolute w-full mt-10"
+    class="group flex flex-row bg-NFTW-black-800 opacity-80 p-5 text-3xl text-white mx-auto py-4 md:py-4 md:flex-row absolute w-full mt-10"
   >
-    <a href="/"><p>NFTW Adventures</p></a>
-    <a
-      href="/"
-      class="ml-48 pt-1 text-lg hover:scale-125 hover:transition-all hover:text-blue-600 ease-in-out duration-300"
-      >Home</a
-    >
-    <a
-      href="/test"
-      class="ml-12 pt-1 text-lg hover:scale-125 hover:transition-all hover:text-blue-600 ease-in-out duration-300"
-      >Test</a
-    >
-    <a
-      href="/test2"
-      class="ml-12 pt-1 text-lg hover:scale-125 hover:transition-all hover:text-blue-600 ease-in-out duration-300"
-      >Test 2</a
-    >
-    <a
-      href="/spacetravel"
-      class="ml-12 pt-1 text-lg hover:scale-125 hover:transition-all hover:text-blue-600 ease-in-out duration-300"
-      >Spacetravel</a
-    >
-    <a
-      href="/clientStories"
-      class="ml-12 pt-1 text-lg hover:scale-125 hover:transition-all hover:text-blue-600 ease-in-out duration-300"
-      >Client stories</a
-    >
-    <a
-      href="/contact"
-      class="ml-12 pt-1 text-lg hover:scale-125 hover:transition-all hover:text-blue-600 ease-in-out duration-300"
-      >Contact</a
-    >
-    <a
-      href="/imprint"
-      class="ml-12 pt-1 text-lg hover:scale-125 hover:transition-all hover:text-blue-600 ease-in-out duration-300"
-      >Imprint</a
-    >
-    <a
-      href="/privacyPolicy"
-      class="ml-12 pt-1 text-lg hover:scale-125 hover:transition-all hover:text-blue-600 ease-in-out duration-300"
-      >Privacy policy</a
-    >
-    <a
-      href="/termsOfUse"
-      class="ml-12 pt-1 text-lg hover:scale-125 hover:transition-all hover:text-blue-600 ease-in-out duration-300"
-      >Terms of use</a
-    >
+    <div class="mx-auto max-w-7xl px-6 lg:px-8 xl:grid xl:grid-cols-2">
+      <a href="/"><p>NFTW Adventures</p></a>
+      <div
+        class="grid grid-cols-3 gap-8 justify-self-center sm:justify-self-end"
+      >
+        <a
+          href="/"
+          class="pt-1 text-lg hover:scale-125 hover:transition-all hover:text-NFTW-blue-400 ease-in-out duration-300"
+          >Home</a
+        >
+        <a
+          href="/spacetravel"
+          class="pt-1 text-lg hover:scale-125 hover:transition-all hover:text-NFTW-blue-400 ease-in-out duration-300"
+          >Spacetravel</a
+        >
+        <a
+          href="/clientStories"
+          class="pt-1 text-lg hover:scale-125 hover:transition-all hover:text-NFTW-blue-400 ease-in-out duration-300"
+          >Client stories</a
+        >
+      </div>
+    </div>
   </div>
 </div>
 
