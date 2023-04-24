482b20d18087385794752bf513e5509cde8155b7
danielrittrich <danielrittrich@web.de>
2023-04-24 18:50:56 +0200

    test

--- src/lib/components/Card2.svelte
+++ src/lib/components/Card2.svelte
@@ -20,7 +20,7 @@
   </p>
   <a href="#" aria-describedby="tier-startup" class="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 bg-indigo-600 text-white shadow-sm hover:bg-indigo-500">Buy plan</a>
   <ul role="list" class="mt-8 space-y-3 text-sm leading-6 text-gray-600 xl:mt-10">
-    <li class="flex gap-x-3 group-hover:text-NFTW-pink-400">
+    <li class="flex gap-x-3 hover:text-NFTW-pink-400">
       <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
         <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
       </svg>
