1a5d09c921d3e52508b0aea873e6051572c56c4f
danielrittrich <danielrittrich@web.de>
2023-04-24 16:45:44 +0200

    test

--- src/routes/[[sites]]/test2/+page.svelte
+++ src/routes/[[sites]]/test2/+page.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
     // your script goes here
+
+    import Card2 from "$lib/components/Card2.svelte";
+    import SectionBg001 from "$lib/components/SectionBg001.svelte";
+
+
 </script>
 
 <div>
     <!-- your html goes here -->
     Hallo Le Xuan
+
+    <SectionBg001 title1of2="Hallo DU DA" caption="Test">
+        <Card2></Card2>
+        <Card2></Card2>
+    </SectionBg001>
+
 </div>
