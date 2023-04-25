c5834a7a2f3f90f69c216d1f05e69616ea00a312
DanielRittrich <danielrittrich@web.de>
2023-04-26 00:33:31 +0200

    Component: SectionBg002uni -> universal version

---
+++ src/lib/components/SectionBg002uni.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { randomPercentage } from "$lib/tools/Tools.svelte";
+</script>
+
+<div class="relative isolate bg-NFTW-bg px-6 py-24 sm:py-32 lg:px-8">
+  <div
+    class="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl"
+    aria-hidden="true"
+  >
+    <div
+      class="mx-auto aspect-[1155/678] w-full bg-gradient-to-tr from-NFTW-pink-300 to-NFTW-blue-400 opacity-30"
+      style="clip-path: polygon({randomPercentage()} {randomPercentage()}, 100% 61.6%, 97.5% 26.9%, {randomPercentage()} 0.1%, 80.7% 2%, 72.5% 32.5%, {randomPercentage()} 62.4%, 52.4% 68.1%, 47.5% 58.3%, {randomPercentage()} 34.5%, 27.5% 76.7%, 0.1% 64.9%, {randomPercentage()} 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+    />
+  </div>
+  <div
+    class="mx-auto w-full mt-16 items-center sm:mt-20 sm:gap-y-0"
+  >
+    <slot />
+  </div>
+</div>
+
+<style>
+</style>
