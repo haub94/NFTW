f30cf97b910a1209e6f89e800674e85a4df308f6
DanielRittrich <danielrittrich@web.de>
2023-04-29 00:05:57 +0200

    iframes test

--- src/routes/[[sites]]/getInContact/+page.svelte
+++ src/routes/[[sites]]/getInContact/+page.svelte
@@ -2,20 +2,19 @@
   import Form from "$lib/sectionComponents/Form.svelte";
   import DividerTextImage from "$lib/sectionComponents/DividerTextImage.svelte";
   import SectionBg002 from "$lib/sectionComponents/SectionBg002.svelte";
-  let dividerCaption:string="caption";
+  let dividerCaption: string = "Get ready for take off";
 </script>
 
 <div class="pt-60 bg-NFTW-bg" />
 
-<SectionBg002>
-  <Form />
-</SectionBg002>
 <DividerTextImage
-title="Title"
-caption={dividerCaption}
-img="/planets/red-planet.png"
-imgSize="cover"
-imgPosition="center"
+  title="You are on the finish line"
+  caption={dividerCaption}
+  img="/astronauts/astronautKid.png"
+  imgSize="cover"
+  imgPosition="center"
+  imgHeight="50vh"
 />
 <SectionBg002>
+  <Form />
 </SectionBg002>
