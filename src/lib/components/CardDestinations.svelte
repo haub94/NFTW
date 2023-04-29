5af7424e8de155a30fe5ab5c7be2a4392f261d3a
Lelo1094 <105745082+Lelo1094@users.noreply.github.com>
2023-04-29 12:39:53 +0200

    Component and cards for Destination and Journey p.

---
+++ src/lib/components/CardDestinations.svelte
@@ -0,0 +1,20 @@
+<script lang="ts"> 
+  
+</script>
+
+
+
+
+<div class="group rounded-3xl p-8 xl:p-10 ring-2 bg-NFTW-black-500">
+  <div class="flex items-center justify-between gap-x-4">
+    <h1 id="tier-startup" class="text-lg font-semibold leading-8 text-NFTW-blue-50">MOON</h1>
+  </div>
+  <p class="mt-4 text-sm leading-6 text-NFTW-blue-50">From the moon and back.</p>
+  <p class="mt-6 flex items-baseline gap-x-1">
+    <span class="text-4xl font-bold tracking-tight text-NFTW-blue-50">$450,000</span>
+  </p>
+  <a href="#" aria-describedby="tier-startup" class="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 bg-indigo-600 text-white shadow-sm hover:bg-indigo-500">Get more details</a>
+  </div>
+
+  
+    
--- src/routes/[[sites]]/spacetravel/destination/+page.svelte
+++ src/routes/[[sites]]/spacetravel/destination/+page.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
     // your script goes here
+
+  import CardDestinations from "$lib/components/CardDestinations.svelte";
+  import SectionBg001 from "$lib/sectionComponents/SectionBg001.svelte";
+
+
+    
 </script>
 
 <div>
     <!-- your html goes here -->
+  <SectionBg001
+  title1of2="Destinations"
+  caption=""
+  >
+    <CardDestinations></CardDestinations>
+    <CardDestinations></CardDestinations>
+    <CardDestinations></CardDestinations>
+  </SectionBg001>
 </div>
