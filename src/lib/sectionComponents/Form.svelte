a159dafa8f4e2fbbe613be3f3e99e8004f866aa5
Markus Haubold <105038675+haub94@users.noreply.github.com>
2023-05-01 19:31:29 +0200

    set overrideMemory false

--- src/lib/sectionComponents/Form.svelte
+++ src/lib/sectionComponents/Form.svelte
@@ -14,7 +14,7 @@
   const dateStyle = "block w-full mt-2.5 rounded-md border-0 bg-white/5 px-3.5 py-2 text-NFTW-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-NFTW-blue-500 sm:text-sm sm:leading-6";
   const selectionStyling = "bg-NFTW-black-600 bg-opacity-90";
 
-  const overrideMemory: boolean = true; //DEV-ONLY
+  const overrideMemory: boolean = false; //DEV-ONLY
   const empty: string = "";
 
   //helper to override the memory during dev
