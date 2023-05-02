5ff41288d3248e85b2987aaf46f71bb733feea4d
Markus Haubold <105038675+haub94@users.noreply.github.com>
2023-05-02 23:22:29 +0200

    arrange cards

--- src/lib/components/StorieCard.svelte
+++ src/lib/components/StorieCard.svelte
@@ -3,9 +3,9 @@
 	import FaUsers from 'svelte-icons/fa/FaUsers.svelte';
 	import FaGrav from 'svelte-icons/fa/FaGrav.svelte';
 
-	export let destination: string = "Moon";
+	export let destination: string = "Mars";
 	export let purpose: string = "Honeymoon";
-	export let passengers: string = "99";
+	export let completedTrips: string = "99";
 
 
 	let destinationImage: string = "";
@@ -13,6 +13,12 @@
 	switch (destination) {
 		case "Moon":
 			destinationImage = "/planetsWithoutBg/moon.webp";
+			break;
+		case "Mars":
+			destinationImage = "/planetsWithoutBg/mars.webp";
+			break;
+		case "Venus":
+			destinationImage = "/planetsWithoutBg/venus.webp";
 			break;
 	
 		default:
@@ -31,17 +37,20 @@
 			icon: FaGrav
 		},
 		{
-			header: "passengers",
-			value: passengers,
+			header: "Completed trips",
+			value: completedTrips,
 			icon: FaUsers
 		}
 	]
 
 </script>
 
-<div class="flex flex-col bg-NFTW-blue-100 p-2 rounded-xl border-2 border-NFTW-blue-300">
+<div class="flex flex-col bg-NFTW-blue-900 p-2 rounded-xl border-2 border-NFTW-blue-700 w-80 mb-12">
 	<!--image-->
-	<div class="bg-no-repeat bg-contain bg-center w-full h-96 " style="background-image: url({destinationImage})"></div>
+	<div class="w-full p-4" class:p-4={destination != "Moon"}> <!--moon is smaller than the other, so give the other padding-->
+		<div class="bg-no-repeat bg-contain bg-center w-full h-96" style="background-image: url({destinationImage})"></div>
+	</div>
+	
 	<!--stats-->
 	{#each statsTemplate as stat}
 	<div class="">
@@ -51,10 +60,10 @@
 				<div class="absolute w-12 h-12 rounded-md bg-NFTW-lila-800 text-NFTW-lila-300 p-1">
 					<svelte:component this={stat.icon} />
 				</div>
-				<p class="ml-16 truncate text-sm font-medium text-gray-500 uppercase">{stat.header}</p>
+				<p class="ml-16 truncate text-sm font-medium text-NFTW-black-500 uppercase">{stat.header}</p>
 			</div>
 			<div class="ml-16 flex items-baseline">
-				<p class="text-2xl font-semibold text-gray-900">{stat.value}</p>
+				<p class="text-2xl font-semibold text-NFTW-black-500">{stat.value}</p>
 				
 			</div>
 			</div>
--- src/lib/sectionComponents/Stories.svelte
+++ src/lib/sectionComponents/Stories.svelte
@@ -1,9 +1,46 @@
 <script>
     import StorieCard from "$lib/components/StorieCard.svelte";
 
+    const cardData = [
+        {
+            destination: "Venus",
+            purpose: "Honeymoon",
+            completedTrips: "23"
+        },
+        {
+            destination: "Mars",
+            purpose: "Vacation",
+            completedTrips: "49"
+        },
+        {
+            destination: "Moon",
+            purpose: "Birthdayspecial",
+            completedTrips: "11"
+        },
+        {
+            destination: "Mars",
+            purpose: "Photosafarie",
+            completedTrips: "36"
+        },
+        {
+            destination: "Venus",
+            purpose: "Saisonal Offer",
+            completedTrips: "9"
+        },
+    ]
     
 </script>
 
-<div class="my-10">
-    <StorieCard />
+<div class="mb-28 flex flex-col md:flex-row md:flex-wrap w-full md:justify-around">
+    
+    {#each cardData as card}
+        <div class="mx-auto">
+            <StorieCard 
+            destination={card.destination}
+            purpose={card.purpose}
+            completedTrips={card.completedTrips}
+        />
+        </div>
+        
+    {/each}
 </div>
