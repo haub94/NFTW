45e040488e1937be8c578f3be86003eb2b41e895
Markus Haubold <105038675+haub94@users.noreply.github.com>
2023-05-09 22:53:56 +0200

    change german to englisch

--- src/routes/[[sites]]/privacyPolicy/+page.svelte
+++ src/routes/[[sites]]/privacyPolicy/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import SectionHeader from "$lib/components/SectionHeader.svelte";
   import SectionBg002 from "$lib/sectionComponents/SectionBg002.svelte";
-  import DatenschutzContent from "$lib/sectionComponents/DatenschutzContent.svelte";
+  import PrivacyPolicyContent from "$lib/sectionComponents/PrivacyPolicyContent.svelte"
 
 </script>
 
 <SectionBg002>
-  <SectionHeader title="Privacy Policy" />
-  <DatenschutzContent />
+  <SectionHeader title="Privacy Policy" description="Information requirements according to the General Data Protection Regulation (DSGVO)" />
+  <PrivacyPolicyContent />
 </SectionBg002>
