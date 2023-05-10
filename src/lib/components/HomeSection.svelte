b6094f3697b3dd37f8c5a4aaa4961c0394ce8fa9
Lelo1094 <105745082+Lelo1094@users.noreply.github.com>
2023-05-11 00:41:56 +0200

    Add components for Homepage

--- src/lib/components/HomeSection.svelte
+++ src/lib/components/HomeSection.svelte
@@ -2,15 +2,24 @@
     
 </script>
 
-<div class="bg-NFTW-black-500 text-NFTW-blue-50 py-20">
-    <div class="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
-        <div class="h-48 flex flex-wrap content-center">
-            <div>
-                <img class="inline-block mt-28 hidden xl:block" src="/home/rocketPill.png"></div>
-            <div>
-                <img class="inline-block mt-24 md:mt-0 p-8 md:p-0"  src="/home/astronautPill.png"></div>
-            <div>
-                <img class="inline-block mt-28 hidden lg:block" src="/home/spacecraftPill.png"></div>
+
+<div class="bbg-gradient-to-tr from-NFTW-pink-300 to-NFTW-blue-400 text-NFTW-blau50 text-NFTW-blue-50 py-20">
+    <div class="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
+        <div class="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
+            <h2 class="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">What we offer
+            </h2>
+            <p class="text-sm md:text-base text-NFTW-blue-50">We offer a variety of services for tourists who are interested in traveling to space.</p>
+
         </div>
-    </div>
-</div>
+        <div class="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
+            <div class="h-48 flex flex-wrap content-center">
+                <div>
+                    <img class="inline-block mt-28 hidden xl:block" src="/home/spacecraftPill.png"></div>
+                    <div>
+                        <img class="inline-block mt-24 md:mt-0 p-8 md:p-0" src="/home/astronautPill.png"></div>
+                        <div>
+                            <img class="inline-block mt-28 hidden lg:block" src="/home/rocketPill.png"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
--- src/routes/[[sites]]/+page.svelte
+++ src/routes/[[sites]]/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import HomeSection from "$lib/components/HomeSection.svelte";
-import BgVideo from "$lib/sectionComponents/BgVideo.svelte";
+  import BgVideo from "$lib/sectionComponents/BgVideo.svelte";
+  import HomeSectionBg from "$lib/sectionComponents/HomeSectionBg.svelte";
   import SectionBg001 from "$lib/sectionComponents/SectionBg001.svelte";
+  import SectionBg002 from "$lib/sectionComponents/SectionBg002.svelte";
 </script>
 
 <BgVideo
@@ -12,6 +14,20 @@ import BgVideo from "$lib/sectionComponents/BgVideo.svelte";
   videoHeight="70vh"
 />
 
-<SectionBg001
-title1of2="About us"></SectionBg001>
+<HomeSectionBg
+ title1of2="About us"
+ title2of2="">
+   <div class="text-NFTW-blue-50 text-2xl">
+      <p>Welcome to our space tourism company, where we are passionate about providing an unforgettable experience for those who dream of exploring the final frontier. Our mission is to enable individuals to fulfill their lifelong ambition of space travel and to inspire a new generation of space enthusiasts.</p><br>
+      <p>Our company was founded by a team of experienced entrepreneurs, engineers, and space enthusiasts who have a deep love for space exploration. We have partnered with the leading space agencies and private space companies to ensure that our customers have access to the most advanced and safest technologies available.</p>
+  </div>
+</HomeSectionBg>
+  
+
+
 <HomeSection></HomeSection>
+
+<SectionBg002>
+
+</SectionBg002>
+
