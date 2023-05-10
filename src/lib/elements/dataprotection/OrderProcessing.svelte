eb512bb901cf1b2d0232980c2817e0a66710e313
Markus Haubold <105038675+haub94@users.noreply.github.com>
2023-05-10 12:31:21 +0200

    giv y-spacing between title and text

--- src/lib/elements/dataprotection/OrderProcessing.svelte
+++ src/lib/elements/dataprotection/OrderProcessing.svelte
@@ -7,18 +7,21 @@
 	if (text === '') {
 		//use mostly used text
 		textToPrint =
-			'Wir haben einen Vertrag über Auftragsverarbeitung (AVV) zur Nutzung des oben genannten Dienstes geschlossen. Hierbei handelt es sich um einen datenschutzrechtlich vorgeschriebenen Vertrag, der gewährleistet, dass dieser die personenbezogenen Daten unserer Websitebesucher nur nach unseren Weisungen und unter Einhaltung der DSGVO verarbeitet.';
+			'We have concluded a contract on order processing (AVV) for the use of the above-mentioned service. This is a contract required by data protection law, which ensures that this processes the personal data of our website visitors only according to our instructions and in compliance with the GDPR.';
 	} else {
 		textToPrint = text;
 	}
 
 	if (title === '') {
 		//use mostly used text
-		titleToPrint = 'Auftragsverarbeitung';
+		titleToPrint = 'Order Processing';
 	} else {
 		titleToPrint = title;
 	}
 </script>
 
-<h3 class="text-base font-bold italic">{titleToPrint}</h3>
-<p class="pb-4 text-base">{textToPrint}</p>
+<div class="space-y-1">
+	<h3 class="text-nftw-pink-100 font-bold italic">{titleToPrint}</h3>
+	<p class="pb-4 text-base">{textToPrint}</p>
+</div>
+
