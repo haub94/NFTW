c4a07c497bcc4d08772c6e6996d2ba3003ae62cb
Markus Haubold <105038675+haub94@users.noreply.github.com>
2023-05-11 13:10:14 +0200

    cleanup logs

--- src/routes/[[sites]]/getInContact/+page.svelte
+++ src/routes/[[sites]]/getInContact/+page.svelte
@@ -16,30 +16,22 @@
   let JOURNEY_PURPOSE: JourneyPurposeData[];
   ({ JOURNEY_PURPOSE } = data);  //get data from db
 
-  function test() {
   //get the current destination to load the start- /enddate
   for (let index = 0; index < JOURNEY_PURPOSE.length; index++) {
     //iterate to current journey purposes to get current name
     if ((JOURNEY_PURPOSE[index].name === $packageMemory.journeyPurpose)) {
-      console.log('index :>> ', index);
-      console.log('JOURNEY_PURPOSE[index].name :>> ', JOURNEY_PURPOSE[index].name);
       //load start-/enddate if it´s set in the db
       if (!($packageMemory.journeyPurpose === "birthday special") && 
           !($packageMemory.journeyPurpose === "honeymoon")) {
-            console.log("in if");
-            console.log('JOURNEY_PURPOSE[index].startDate; :>> ', JOURNEY_PURPOSE[index].startDate);
-            console.log('JOURNEY_PURPOSE[index].endtDate; :>> ', JOURNEY_PURPOSE[index].endDate);
             $packageMemory.startDate = JOURNEY_PURPOSE[index].startDate;
             $packageMemory.endDate = JOURNEY_PURPOSE[index].endDate;
       }
     }
   }
-}
 
 </script>
 
 <SectionBg002>
-  <button class="text-2xl text-red-500" on:click={test}>fire test</button>
   <SectionHeader title="Get In Contact" />
   <DividerTextImage
     title="You are on the finish line"
