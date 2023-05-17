e3ea1b5fc4cb3768f830034716cb1ca84d952fed
danielrittrich <danielrittrich@web.de>
2023-05-17 12:14:36 +0200

    Bugfixing Startseite und JP

--- src/lib/components/LogoSection.svelte
+++ src/lib/components/LogoSection.svelte
@@ -4,13 +4,13 @@
 
 
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <h1 class="text-NFTW-blue-50 text-2xl text-center font-bold">Trusted by the world’s most innovative teams</h1>
-      <div class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-        <img class="col-span-3 max-h-30 w-full object-contain lg:col-span-1 invert" src="/logos/nasa.png" alt="Nasa" width="158" height="48">
-        <img class="col-span-3 max-h-30 w-full object-contain lg:col-span-1 invert-0" src="/logos/spacex.png" alt="SpaceX" width="158" height="48">
-        <img class="col-span-3 max-h-30 w-full object-contain lg:col-span-1 invert" src="/logos/esa.png" alt="esa" width="158" height="48">
-        <img class="col-span-3 max-h-30 w-full object-contain sm:col-start-2 lg:col-span-1" src="/logos/spaceforce.png" alt="USspaceforce" width="158" height="48">
-        <img class="col-span-3 col-start-2 max-h-30 w-full object-contain sm:col-start-auto lg:col-span-1 invert" src="/logos/starlink.png" alt="Starlink" width="158" height="48">
+      <h1 class="text-NFTW-blue-50 text-2xl text-center font-bold">Trusted by the world's most innovative teams</h1>
+      <div class="mx-auto mt-10 grid max-w-lg grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-items-center items-center gap-x-8 gap-y-10 sm:max-w-xl sm:gap-x-10 lg:mx-0 lg:max-w-none">
+        <img class="col-span-1 max-h-30 w-full object-contain invert" style="max-width:150px" src="/logos/nasa.png" alt="Nasa" width="158" height="48">
+        <img class="col-span-1 max-h-30 w-full object-contain invert-0" style="max-width:150px" src="/logos/spacex.png" alt="SpaceX" width="158" height="48">
+        <img class="col-span-1 max-h-30 w-full object-contain invert" style="max-width:150px" src="/logos/esa.png" alt="esa" width="158" height="48">
+        <img class="col-span-1 max-h-30 w-full object-contain" style="max-width:150px" src="/logos/spaceforce.png" alt="USspaceforce" width="158" height="48">
+        <img class="col-span-1 max-h-30 w-full object-contain invert" style="max-width:150px" src="/logos/starlink.png" alt="Starlink" width="158" height="48">
       </div>
     </div>
   
--- src/routes/[[sites]]/spacetravel/journeyPurpose/+page.svelte
+++ src/routes/[[sites]]/spacetravel/journeyPurpose/+page.svelte
@@ -1,27 +1,42 @@
 <script lang="ts">
   import CardDestinations from "$lib/components/CardDestinations.svelte";
   import SectionHeader from "$lib/components/SectionHeader.svelte";
-  import SectionBg001 from "$lib/sectionComponents/SectionBg001.svelte";
+  import SectionBg002 from "$lib/sectionComponents/SectionBg002.svelte";
   import type { PageData } from "./$types";
   export let data: PageData;
 </script>
 
-<SectionBg001 title1of2="" caption="">
-  <div class="col-span-2">
-    <SectionHeader title="Journey Purposes" />
+<!-- 
+<SectionBg002>
+  <SectionHeader title="Destinations" />
+  <div
+    class="grid justify-center justify-self-center mx-auto max-w-7xl gap-12 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"
+  >
+    {#each data as data}
+      <div class="grid col-span-1">
+
+
+ -->
+
+<SectionBg002>
+  <SectionHeader title="Journey Purposes" />
+  <div
+    class="grid justify-center justify-self-center mx-auto max-w-7xl gap-12 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"
+  >
+    {#each data.JOURNEY_PURPOSES as data}
+      <div class="grid col-span-1">
+        <CardDestinations
+          jpName={data.name}
+          jpPromoText={data.promoText}
+          jpPrice={data.price}
+          routePath={data.routePath}
+          jpImagePath={data.image}
+          imgPosition={"left"}
+          imgSize={"cover"}
+          jpImageName={""}
+          jpImageAltText={""}
+        />
+      </div>
+    {/each}
   </div>
-  {#each data.JOURNEY_PURPOSES as data}
-    <div class="col-span-1">
-      <CardDestinations
-        jpName={data.name}
-        jpPromoText={data.promoText}
-        jpPrice={data.price}
-        routePath={data.routePath}
-        jpImagePath={data.image}
-        imgPosition={'left'}
-        imgSize={'cover'}
-        jpImageName={''}
-        jpImageAltText={''} />
-    </div>
-  {/each}
-</SectionBg001>
+</SectionBg002>
