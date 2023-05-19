9a8730b012dbc7ccaed561e594a2022a74065caa
danielrittrich <danielrittrich@web.de>
2023-05-19 15:44:38 +0200

    Buttons y-padding und height

--- src/lib/components/ButtonsDestJpGic.svelte
+++ src/lib/components/ButtonsDestJpGic.svelte
@@ -73,7 +73,7 @@
 </script>
 
 <div
-  class="relative w-full grid justify-center bg-gradient-to-b from-NFTW-bg to-transparent"
+  class="relative w-full grid justify-center bg-gradient-to-b from-NFTW-bg to-transparent px-8"
   class:border-red-500={IS_PROTOTYPING}
   class:border-4={IS_PROTOTYPING}
   class:bg-NFTW-white={IS_PROTOTYPING}
@@ -90,13 +90,13 @@
       id="buttonDestinationJourneyPurpose"
       href={routeButtonDestinationJourneyPurpose}
       aria-describedby="link-DestinationOrJourneyPurpose"
-      class="h-14 px-7 grid items-center rounded-md col-span-1 bg-NFTW-lila-800 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-NFTW-lila-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition duration-500"
+      class="h-auto min-h-full px-7 grid items-center rounded-md col-span-1 bg-NFTW-lila-800 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-NFTW-lila-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition duration-500"
       class:border-4={IS_PROTOTYPING}
       class:border-yellow-500={IS_PROTOTYPING}
       class:text-NFTW-white={!IS_PROTOTYPING}
       >
         {PAGE_IS_DESTINATION
-          ? "Select current destination and go to Journey Purposes"
+          ? "Select current destination and go to Journey xxxxxxxx xxxxxxx xxxxxxxx xxxxxxxxx xxxxxxxxx xxxxxxxxx xxxxxxxx xxxxxxxxx xxxxxxxx xxxxxxx Purposes"
           : PAGE_IS_JOURNEY_PURPOSE
           ? "Select current destination and go to Destination"
           : "none"}
@@ -109,13 +109,13 @@
       id="buttonGetInContact"
       href={ROUTE_BUTTON_GIC}
       aria-describedby="link-GetInContact"
-      class="h-14 px-7 rounded-md col-span-1 bg-NFTW-lila-800 py-2 text-center items-center grid text-sm font-semibold leading-6 text-white shadow-sm hover:bg-NFTW-lila-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition duration-500"
+      class="h-auto min-h-full px-7 rounded-md col-span-1 bg-NFTW-lila-800 py-2 text-center items-center grid text-sm font-semibold leading-6 text-white shadow-sm hover:bg-NFTW-lila-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition duration-500"
       class:border-4={IS_PROTOTYPING}
       class:border-orange-500={IS_PROTOTYPING}
       class:text-NFTW-white={!IS_PROTOTYPING}>Start Your Journey With Us</a>
     {:else}
     <div
-      class="h-14 px-7 rounded-md col-span-1 bg-gray-600 py-2 text-center items-center grid text-sm font-semibold leading-6 text-white shadow-sm"
+      class="h-auto min-h-full px-7 rounded-md col-span-1 bg-gray-600 py-2 text-center items-center grid text-sm font-semibold leading-6 text-white shadow-sm"
       class:border-4={IS_PROTOTYPING}
       class:border-orange-500={IS_PROTOTYPING}
       class:text-NFTW-white={!IS_PROTOTYPING}>
