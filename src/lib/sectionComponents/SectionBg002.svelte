ec314db76c7feff5f447d7f58d181ce4f8851240
danielrittrich <danielrittrich@web.de>
2023-05-19 15:02:34 +0200

    DetailContent randlos

--- src/lib/sectionComponents/SectionBg002.svelte
+++ src/lib/sectionComponents/SectionBg002.svelte
@@ -6,7 +6,10 @@
   export let paddingBottom: string = "128px";
 </script>
 
-<div class="relative isolate bg-NFTW-bg px-6 lg:px-8"style="padding-top:{paddingTop} ; padding-bottom:{paddingBottom};">
+<div
+  class="relative isolate bg-NFTW-bg"
+  style="padding-top:{paddingTop} ; padding-bottom:{paddingBottom};"
+>
   <div
     class="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl"
     aria-hidden="true"
@@ -16,14 +19,14 @@
       style="clip-path: polygon({randomPercentage()} {randomPercentage()}, 100% 61.6%, 97.5% 26.9%, {randomPercentage()} 0.1%, 80.7% 2%, 72.5% 32.5%, {randomPercentage()} 62.4%, 52.4% 68.1%, 47.5% 58.3%, {randomPercentage()} 34.5%, 27.5% 76.7%, 0.1% 64.9%, {randomPercentage()} 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
     />
   </div>
-  
-    <!-- universal with full width -->
-    {#if !twoCols}
-      <div class="mx-auto w-full items-center sm:gap-y-0" >
-        <slot />
-      </div>
-    {/if}
-  
+
+  <!-- universal with full width -->
+  {#if !twoCols}
+    <div class="mx-auto w-full items-center sm:gap-y-0">
+      <slot />
+    </div>
+  {/if}
+
   <!-- 2 cols -->
   {#if twoCols}
     <div
