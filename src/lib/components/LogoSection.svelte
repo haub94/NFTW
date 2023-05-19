a7276a212019a74e31de3717f7ac351394488eff
danielrittrich <danielrittrich@web.de>
2023-05-19 14:17:22 +0200

    Logo responsive design neu

--- src/lib/components/LogoSection.svelte
+++ src/lib/components/LogoSection.svelte
@@ -1,16 +1,42 @@
 <script lang="ts">
-
 </script>
 
-
-    <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <h1 class="text-NFTW-blue-50 text-2xl text-center font-bold">Trusted by the world's most innovative teams</h1>
-      <div class="mx-auto mt-10 grid max-w-lg grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-items-center items-center gap-x-8 gap-y-10 sm:max-w-xl sm:gap-x-10 lg:mx-0 lg:max-w-none">
-        <img class="col-span-1 max-h-30 w-full object-contain invert" style="max-width:150px" src="/logos/nasa.png" alt="Nasa" width="158" height="48">
-        <img class="col-span-1 max-h-30 w-full object-contain invert-0" style="max-width:150px" src="/logos/spacex.png" alt="SpaceX" width="158" height="48">
-        <img class="col-span-1 max-h-30 w-full object-contain invert" style="max-width:150px" src="/logos/esa.png" alt="esa" width="158" height="48">
-        <img class="col-span-1 max-h-30 w-full object-contain" style="max-width:150px" src="/logos/spaceforce.png" alt="USspaceforce" width="158" height="48">
-        <img class="col-span-1 max-h-30 w-full object-contain invert" style="max-width:150px" src="/logos/starlink.png" alt="Starlink" width="158" height="48">
-      </div>
-    </div>
-  
+<div class="mx-auto max-w-7xl px-6 lg:px-8">
+  <h1 class="text-NFTW-blue-50 text-2xl text-center font-bold">
+    Trusted by the world's most innovative teams
+  </h1>
+  <div
+    class="mx-auto mt-10 grid max-w-lg grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 justify-items-center items-center gap-x-8 gap-y-10 sm:max-w-xl sm:gap-x-10 lg:mx-0 lg:max-w-none"
+  >
+    <img
+      class="col-span-1 max-h-30 w-full object-contain invert"
+      style="max-width:150px"
+      src="/logos/nasa.png"
+      alt="Nasa"
+    />
+    <img
+      class="col-span-1 max-h-30 w-full object-contain invert brightness-0"
+      style="max-width:200px"
+      src="/logos/spacex.png"
+      alt="SpaceX"
+    />
+    <img
+      class="col-span-1 max-h-30 w-full object-contain invert"
+      style="max-width:130px"
+      src="/logos/esa.png"
+      alt="esa"
+    />
+    <img
+      class="col-span-1 max-h-30 w-full object-contain"
+      style="max-width:130px"
+      src="/logos/spaceforce.png"
+      alt="USspaceforce"
+    />
+    <img
+      class="col-span-1 max-h-30 w-full object-contain invert"
+      style="max-width:150px"
+      src="/logos/starlink.png"
+      alt="Starlink"
+    />
+  </div>
+</div>
